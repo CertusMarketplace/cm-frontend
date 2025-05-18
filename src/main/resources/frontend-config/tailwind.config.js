4242b4c99342dc2ac30872cfceb95714edd51953
LucianMarquina <alemarquina789@gmail.com>
2025-05-18 15:14:11 -0500

    Variables y Confi completas | Fuentes y Variables de Colores listas para su uso en el desarrollo web

--- src/main/resources/frontend-config/tailwind.config.js
+++ src/main/resources/frontend-config/tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
    "../pages/**/templates/**/*.html",  
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blueCertus: "#0056A0",
+        darkBlueCertus: "#00205B",
+        skyBlueCertus: "#2FBAD9",
+        redCertus: "#F24968",
+        purpleCertus: "#A64B98",
+        greenCertus: "#00A08D"
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        righteous: ["Righteous", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
